768669f7d1fa1d4c2f20cbca85e525c36648f44c
Arya Emami <aryaemami59@yahoo.com>
2022-10-16 01:52:56 -0500

    clean react app

---
+++ src/features/redux/store.ts
@@ -0,0 +1,22 @@
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+// import { addedReducer } from "../Redux/addedSlice";
+
+export const store = configureStore({
+  reducer: {
+    // added: addedReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      thunk: true,
+      serializableCheck: false,
+      immutableCheck: false,
+    }),
+});
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
